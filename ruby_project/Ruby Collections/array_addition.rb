65e864d8c880abac682d8a17ed6134b5ce969fb3
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2017-07-15 21:53:28 -0700

    finished project for ruby

--- ruby_project/Ruby Collections/array_addition.rb
+++ ruby_project/Ruby Collections/array_addition.rb
@@ -1,3 +1,8 @@
 grocery_list = ["milk", "eggs", "bread"]
+grocery_list << "carrots"
+grocery_list.push("potatoes")
+grocery_list.unshift("celery")
+grocery_list += ["ice cream", "pie"]
+
 
 puts grocery_list.inspect
