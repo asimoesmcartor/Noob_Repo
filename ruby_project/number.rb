7b7a5875fd388139360183c7c0c4f4fbe4c28534
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2017-07-15 19:11:23 -0700

    adding updates of new methods for number.rb

---
+++ ruby_project/number.rb
@@ -0,0 +1,28 @@
+def get_name()
+  print "Enter your name: "
+  return gets.chomp
+end
+
+def greet(name)
+  puts "Hi #{name}!"
+  if (name == "Alex")
+    puts "That's a great name!"
+  end
+end
+
+def get_number(number)
+  print "What number would you like to test? "
+  return gets.chomp.to_i
+end
+
+def divisible_by_3?(number)
+  return (number % 3 == 0)
+end
+
+name = get_name()
+greet(name)
+number = get_number()
+
+if divisible_by_3?(number)
+  puts "Your number is divisible by 3!"
+end
