ee7157bed1d7afdd96248a97cc1c1a384cba28a4
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2017-07-15 19:10:53 -0700

    adding updates of new methods for number.rb

--- ruby_project/car.rb
+++ ruby_project/car.rb
@@ -4,3 +4,7 @@ car2_speed = 60
 if !(car1_speed == car2_speed)
   puts "Car 1 and Car 2 are going the same speed"
 end
+
+if (car1_speed > 40) && (car1_speed > car2_speed)
+  puts "Car 1 is going fast, but not as fast as car 2."
+end
