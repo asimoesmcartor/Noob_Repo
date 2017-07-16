64cf9096413f0f636c434691ea2c5ca4e9c09509
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2017-07-15 22:47:50 -0700

    added full hash program experiment

--- ruby_project/Ruby Collections/hash_methods.rb
+++ ruby_project/Ruby Collections/hash_methods.rb
@@ -5,3 +5,27 @@ hash = {
 }
 
 puts "Hash: #{hash.inspect}"
+
+print "hash.length: "
+puts hash.length
+
+print "hash.invert: "
+puts hash.invert
+
+print "hash.shift: "
+puts has.shift.inspect
+print "hash: "
+puts hash.inspect
+
+hash["item"] = "Bread"
+
+puts "Hash merging: "
+
+print "Original hash: "
+puts hash.inspect
+
+print "Merged with {'calories' => 100}"
+puts hash.merge({ 'calories' => 100 })
+
+print "Original hash: "
+puts hash.inspect
