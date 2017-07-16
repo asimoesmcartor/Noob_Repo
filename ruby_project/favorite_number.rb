563b6d7e4652e80d98013d81d7030cf39cd11493
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2017-07-15 19:00:18 -0700

    adding updates to fav number project and adding new card project

--- ruby_project/favorite_number.rb
+++ ruby_project/favorite_number.rb
@@ -3,4 +3,6 @@ number = gets.chomp.to_i
 
 if (number == 3) || (number == 5)
   puts "That's by favorite number!"
+elsif (number > 10) && (number.even?)
+  puts "That's a pretty high even number!"
 end
