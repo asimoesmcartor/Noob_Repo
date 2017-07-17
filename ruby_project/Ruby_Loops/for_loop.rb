d267e4ce5a0e77719e469e2aa3f957e48464f5dd
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2017-07-16 21:52:20 -0700

    added new each, hash and times files

---
+++ ruby_project/Ruby_Loops/for_loop.rb
@@ -0,0 +1,3 @@
+for item in 1..10 do
+  puts "The current item is #{item}."
+end
---
+++ ruby_project/Ruby_Loops/hash_iteration.rb
@@ -0,0 +1,5 @@
+business = {"name" => "Treehouse", "location" => "Portland, OR"}
+
+business.each do |key, value|
+  puts "The hash key is #{key} and the value is #{value}."
+end
