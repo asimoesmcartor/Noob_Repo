6f03cf9cd8aa3410240b6a194d446fdeb660cb59
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2017-12-10 16:11:52 -0800

    added next.js testing project. played around with next.js to see how it works. Super fun and will check it out later.

---
+++ Projects/nextjs_project/hello-next/.next/dist/pages/_error.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports = require('next/dist/lib/error.js');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvcGFnZXMvX2Vycm9yLmpzIl0sIm5hbWVzIjpbIm1vZHVsZSIsImV4cG9ydHMiLCJyZXF1aXJlIl0sIm1hcHBpbmdzIjoiOztBQUFBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJfZXJyb3IuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2FsZXhzaW1vZXMtbWNhcnRvci9ub29iX3JlcG8vUHJvamVjdHMvbmV4dGpzX3Byb2plY3QvaGVsbG8tbmV4dCJ9
