cc994d118ac5640798c7a03b3a24ea052ca7de83
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2018-02-11 00:27:40 -0800

    adding new react router project files to new_feature branch

---
+++ Projects/react/react_router_project/project_files/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="main-content not-found">
+    <i className="material-icons icn-error">error_outline</i>
+    <h2>Page Not Found</h2>
+    <p>It seems like the page that you are trying to reach doesn't exist <span role="img">😳</span></p>
+  </div>
+);
+
+export default NotFound;
